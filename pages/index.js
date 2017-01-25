412c125def7b8007890f52caa960eaf843e13b49
yesobo <jesus.espejo.arnas@gmail.com>
2017-01-25 22:01:55 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import Helmet from 'react-helmet'
+import { config } from 'config'
+
+export default class Index extends React.Component {
+  render () {
+    return (
+      <div>
+        <Helmet
+          title={config.siteTitle}
+          meta={[
+            {"name": "description", "content": "Sample"},
+            {"name": "keywords", "content": "sample, something"},
+          ]}
+        />
+        <h1>
+          ¡ Bienvenido ! 
+        </h1>
+        <p>Has llegado al blog de Bankia AAD Front</p>
+        <h2>Nuestros artículos</h2>
+        {/* <h3>Nuestros Últimos artículos</h3> */}
+        <ul>
+          <li>
+            <Link to={prefixLink('/gitflow/')}>Git Flow vs Plan de Gestión de la Configuración</Link>
+          </li>
+        {/*
+          <li>
+            <Link to={prefixLink('/react/')}>JSX (React components)</Link>
+          </li>
+          <li>
+            <Link to={prefixLink('/coffee-react/')}>CJSX (Coffeescript React components)</Link>
+          </li>
+          <li>
+            <Link to={prefixLink('/html/')}>HTML</Link>
+          </li>
+          <li>
+            <Link to={prefixLink('/json/')}>JSON</Link>
+          </li>
+          <li>
+            <Link to={prefixLink('/yaml/')}>YAML</Link>
+          </li>
+          <li>
+            <Link to={prefixLink('/toml/')}>TOML</Link>
+          </li>
+        */}
+        </ul>
+        {/*
+        <h3>Supported CSS processors</h3>
+        <ul>
+          <li>
+            <Link to={prefixLink('/postcss/')}>PostCSS</Link>
+          </li>
+          <li>
+            <Link to={prefixLink('/css-modules/')}>CSS Modules</Link>
+          </li>
+          <li>
+            <Link to={prefixLink('/sass/')}>Sass</Link>
+          </li>
+          <li>
+            <Link to={prefixLink('/less/')}>Less</Link>
+          </li>
+        </ul>
+        */}
+      </div>
+    )
+  }
+}
